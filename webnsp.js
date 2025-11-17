2d678a512e77e0282464e9baf1e76db4f7a27b80
Triasfle <gitarivo.23106@mhs.unesa.ac.id>
2025-11-17 19:28:56 +0700

    testing webserver

--- webnsp.js
+++ webnsp.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
 app.get('/', (req,res) => res.send('Testing Web BNSP'));
+console.log("Update CI/CD Test")
 const port = process.env.PORT || 3000;
 app.listen(port, ()=> console.log('Listening on', port));
